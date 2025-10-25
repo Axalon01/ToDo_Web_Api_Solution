5f5868c0fc6912b8cc9a12f091c36beac3492f9e
Axalon01 <versalife2@gmail.com>
2025-10-24 21:28:14 -0400

    Add Swagger
    - Added Swagger NuGet packages and some lines of code in Program.cs to enable it

--- ToDo_Web_Api/Program.cs
+++ ToDo_Web_Api/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Builder;
 using ToDo_Web_Api.Services;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,13 +9,16 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 builder.Services.AddSingleton<TaskService>();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
