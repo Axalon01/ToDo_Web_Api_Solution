f6ccac8d0b129ea5ff24e1cac7496973147a1d3e
Axalon01 <versalife2@gmail.com>
2025-10-24 20:48:14 -0400

    Fill out TasksController
    - Added TaskService Singleston to Program.cs
    - Filled out the methods in TasksController.cs

--- ToDo_Web_Api/Controllers/TasksController.cs
+++ ToDo_Web_Api/Controllers/TasksController.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using ToDo_Web_Api.Models;
+using ToDo_Web_Api.Services;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -8,36 +11,72 @@ namespace ToDo_Web_Api.Controllers
 	[ApiController]
 	public class TasksController : ControllerBase
 	{
+		private readonly TaskService _taskService;
+		public TasksController(TaskService taskService)
+		{
+			_taskService = taskService;
+		}
+
 		// GET: api/<TasksController>
 		[HttpGet]
-		public IEnumerable<string> Get()
+		public IEnumerable<TaskModel> Get()
 		{
-			return new string[] { "value1", "value2" };
+			return _taskService.LoadTasks();
 		}
 
 		// GET api/<TasksController>/5
 		[HttpGet("{id}")]
-		public string Get(int id)
+		public IActionResult Get(Guid id)
 		{
-			return "value";
+			var tasks = _taskService.LoadTasks().FirstOrDefault(t => t.Id == id);
+
+			if (tasks == null)
+			{
+				return NotFound();
+			}
+
+			return Ok(tasks);
 		}
 
 		// POST api/<TasksController>
 		[HttpPost]
-		public void Post([FromBody] string value)
+		public ActionResult<TaskModel> Post([FromBody] TaskModel value)
 		{
+			var createdTask = _taskService.AddTask(value);
+			return CreatedAtAction(nameof(Get), new { id = createdTask.Id }, createdTask);
 		}
 
 		// PUT api/<TasksController>/5
 		[HttpPut("{id}")]
-		public void Put(int id, [FromBody] string value)
+		public IActionResult Put(Guid id, [FromBody] TaskModel value)
 		{
+			if (id != value.Id)
+			{
+				return BadRequest("ID in URL does not match ID in task body.");
+			}
+
+			var success = _taskService.UpdateTask(value);
+
+			if (!success)
+			{
+				return NotFound();
+			}
+
+			return NoContent(); // 204 - Successfully updated, but nothing to return
 		}
 
 		// DELETE api/<TasksController>/5
 		[HttpDelete("{id}")]
-		public void Delete(int id)
+		public IActionResult Delete(Guid id)
 		{
+			var success = _taskService.DeleteTask(id);
+
+			if (!success)
+			{
+				return NotFound();
+			}
+
+			return NoContent();
 		}
 	}
 }
